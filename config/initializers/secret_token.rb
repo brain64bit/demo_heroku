67d1413b8f5e5c961597eb31441e262347de7107
Agung Prasetyo <brain64bit@gmail.com>
2012-04-02 16:55:05 +0700

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoHeroku::Application.config.secret_token = '0e153d62149230011231de034ff46275250e65d774096987f14acdeba7a60667b0e6968aaa087e9b4c9fa5e1a3dccf248e51738e4f83908e86118296c797b2f7'
